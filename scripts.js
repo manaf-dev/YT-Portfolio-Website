4a2d4cd464c6941666e8bdb45643997807f1a460
rems <tech1.rems@gmail.com>
2023-12-08 23:34:29 +0000

    Add hamburger and hamburger menu

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
-const hamburger = document.querySelector(".toggle_btn");
-const navMenu = document.querySelector(".navbar-links");
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.hamburger_menu');
 
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
+menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+});
