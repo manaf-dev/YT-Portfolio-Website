d740505ff1f828250c5bde75c4b5c9290d2f072e
rems <tech1.rems@gmail.com>
2023-12-23 22:28:03 +0000

    validate the html and added some scripts to js file

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.hamburger_menu');
+const navLinks = document.querySelectorAll('.hamburger_menu .nav-link');
 
-menu_btn.addEventListener('click', function () {
+menu_btn.addEventListener('click', () => {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
+});
+
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobile_menu.classList.remove('is-active');
+    menu_btn.classList.remove('is-active');
+  });
 });
